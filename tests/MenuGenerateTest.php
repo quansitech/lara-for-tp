4a825259c49bf0bdbfe4a9d6ffda19405942e9b4
tider <tider@quansitech.com>
2019-05-13 16:33:22 +0800

    t

---
+++ tests/MenuGenerateTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Larafortp\Tests;
+
+use Larafortp\MenuGenerate;
+
+class MenuGenerateTest extends TestCase{
+
+    public function testInsertAll(){
+//        $data = [];
+//
+//        MenuGenerate::insertAll($data);
+//
+//        $this->assertDatabaseHas('qs_menu', ['title' => 'abc', 'type' => 'backend_menu']);
+
+        $this->assertTrue(true);
+    }
+
+
+}
