d6e61ae0527d7a70129c9ac38a10b64bf0e58a19
tider <164710115@qq.com>
2019-05-15 10:32:24 +0000

    Apply fixes from StyleCI

--- tests/Features/FakerTest.php
+++ tests/Features/FakerTest.php
@@ -1,12 +1,14 @@
 <?php
+
 namespace Larafortp\Tests\Features;
 
 use Faker\Factory as FakerFactory;
 use Larafortp\Tests\TestCase;
 
-class FakerTest extends TestCase{
-
-    public function testZhcn(){
+class FakerTest extends TestCase
+{
+    public function testZhcn()
+    {
         $faker = FakerFactory::create('zh_CN');
         $word = $faker->realText(10);
         $result = preg_match('/[\x{4e00}-\x{9fa5}]/u', $word) ? true : false;
