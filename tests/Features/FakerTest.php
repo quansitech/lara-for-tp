5b054fc41677152a20f68f6c5b354317ac4a7529
tider <tider@quansitech.com>
2019-05-28 09:56:32 +0800

    use placeimg.com for faker image

--- tests/Features/FakerTest.php
+++ tests/Features/FakerTest.php
@@ -14,4 +14,12 @@ class FakerTest extends TestCase
         $result = preg_match('/[\x{4e00}-\x{9fa5}]/u', $word) ? true : false;
         $this->assertTrue($result);
     }
+
+    public function testImage()
+    {
+        $faker = FakerFactory::create('zh_CN');
+        $imageUrl = $faker->imageUrl('1903', '800', 'nature', true, 'sepia');
+        $result = preg_match('/http\:\/\/placeimg\.com\/1903\/800\/nature\/sepia\/\?\d{5}/i', $imageUrl) ? true : false;
+        $this->assertTrue($result);
+    }
 }
