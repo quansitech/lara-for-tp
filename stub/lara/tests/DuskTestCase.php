2b82027e32ffc0240f29a04a9211bbc8b32c039a
tider <tider@quansitech.com>
2019-06-13 16:59:47 +0800

    fix

--- stub/lara/tests/DuskTestCase.php
+++ stub/lara/tests/DuskTestCase.php
@@ -6,12 +6,6 @@ use Testing\DuskTestCase as BaseTestCase;
 
 class DuskTestCase extends BaseTestCase
 {
-
-    protected function laraPath() : string
-    {
-        return __DIR__ . '/..';
-    }
-
     public function setUp() : void
     {
         static::useChromedriver('/usr/local/bin/chromedriver');
